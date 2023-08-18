0243608034107f308a5c77e3d9bf0deb356793ad
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-18 17:02:29 -0300

    [aulasUmaASete] Ganhando legibilidade

--- src/test/java/com/example/testescomrestassured/exercícios/OlaMundoTeste.java
+++ src/test/java/com/example/testescomrestassured/exercícios/OlaMundoTeste.java
@@ -1,5 +1,6 @@
 package com.example.testescomrestassured.exercícios;
 
+import static io.restassured.RestAssured.*;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
@@ -13,7 +14,7 @@ public class OlaMundoTeste {
 
     @Test
     public void testeOlaMundo(){
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
         Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         Assert.assertTrue(response.getStatusCode() == 200);
         assertEquals( 200, response.getStatusCode());
@@ -24,10 +25,17 @@ public class OlaMundoTeste {
 
     @Test
     public void devoConhecerOutrasFormasRestAssured(){
-        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "https://restapi.wcaquino.me/ola");
         ValidatableResponse validacao = response.then().log().all();
         validacao.statusCode(200);
 
-        RestAssured.get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+        get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        given()
+                //pré condições
+        .when() //ações
+                .get("https://restapi.wcaquino.me/ola")
+        .then() //assertivas - validações
+                .statusCode(200);
     }
 }
