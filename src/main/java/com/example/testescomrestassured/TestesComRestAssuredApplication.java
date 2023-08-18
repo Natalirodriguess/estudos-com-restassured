ebe8e6a89999bd2089eec1851a88c6b9f135283c
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-18 11:27:54 -0300

    Iniciando projeto

---
+++ src/main/java/com/example/testescomrestassured/TestesComRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.example.testescomrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesComRestAssuredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestesComRestAssuredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testescomrestassured/TestesComRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testescomrestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesComRestAssuredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
