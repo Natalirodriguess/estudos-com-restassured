60f9ac3604d2636293a31703e9ccb0d6076e58f4
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-18 16:41:39 -0300

    [aulasUmaASete] Iniciando outras formas para usar o rest Assured

--- src/test/java/com/example/testescomrestassured/exercícios/OlaMundoTeste.java
+++ src/test/java/com/example/testescomrestassured/exercícios/OlaMundoTeste.java
@@ -4,16 +4,30 @@ import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class OlaMundoTeste {
 
     @Test
     public void testeOlaMundo(){
         Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString().equals("Ola Mundo!")); //equals retorna true ou false
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.getStatusCode() == 200);
+        assertEquals( 200, response.getStatusCode());
 
         ValidatableResponse validacao = response.then().log().all();
         validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured(){
+        Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then().log().all();
+        validacao.statusCode(200);
+
+        RestAssured.get("https://restapi.wcaquino.me/ola").then().statusCode(200);
     }
 }
